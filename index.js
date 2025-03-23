05361d3c32ebacee688e817b3e79176bc7fe3039
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-03-23 23:59:16 +0300

    declared newheader and removed main element

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code here!
+const element = document.createElement("div");
+
+const ul = document.createElement("h1");
+
+const header = document.createElement("h1");
+header.innerHTML = "Hello World";
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.innerHTML = "Your Name is the champion";  
+
+
+document.body.appendChild(newHeader);
+
+
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
